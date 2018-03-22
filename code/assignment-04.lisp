f5013692e96f240fdcb90f68e974fc281b4c21be
umut <umut.oezge@gmail.com>
2018-03-22 18:31:57 +0300

    sol

---
+++ code/assignment-04.lisp
@@ -0,0 +1,17 @@
+(defun insert-2nd (item lst)
+  (cons (car lst) (cons item (cdr lst)))
+  )
+
+(defun replace-2nd (item lst)
+  (cons (car lst) (cons item (cdr (cdr lst))))
+  )
+
+(defun swap (lst-of-two)
+  (list (cadr lst-of-two) (car lst-of-two))
+  )
+
+(defun wrap-2 (lst)
+  (list (list (car lst)) (list (cadr lst))))
+
+(defun wrap-2 (lst)
+  (cons (cons (car lst) nil) (cons (cons (cadr lst) nil) nil)))
---
+++ code/recursion.lisp
@@ -0,0 +1,41 @@
+(defun factorial (n)
+  (if (zerop n)
+	1
+	(* n (factorial (- n 1)))
+	)
+  )
+
+(defun factorial (n)
+  (cond ((or (not (integerp n)) (< n 0)) (error "Factorial undefined!"))
+		((zerop n) 1)
+		(t (* n (factorial (- n 1))))
+		)
+  )
+
+(defun power (x y)
+  (cond ((zerop y) 1)
+		((= 1 y) x)
+		(t (* x (power x (- y 1))))
+		)
+  )
+
+(defun c-member (item lst)
+  (cond ((endp lst) nil)
+		((equal item (car lst)) lst)
+		(t (c-member item (cdr lst)))
+		)
+  )
+
+(defun range (n)
+  (cond ((or (not (integerp n)) (< n 0)) (error "Range undefined!"))
+		((zerop n) nil)
+		(t (append (range (- n 1)) (list (- n 1))))
+		)
+  )
+
+(defun mapp (func lst)
+  (if (endp lst)
+	nil
+	(cons (funcall func (car lst)) (mapp func (cdr lst)))
+	)
+  )
