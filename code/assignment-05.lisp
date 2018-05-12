458d38ae5a867dd48c8a0628ebc99b8a5bb4dce0
umutozge <umut.oezge@gmail.com>
2018-05-12 19:30:11 +0300

    1526142611

--- code/assignment-05.lisp
+++ code/assignment-05.lisp
@@ -2,30 +2,30 @@
   ;; you may choose how to interpret the unit parameter
   ;; here we take it to be the base of the conversion
   (if (equal 'c unit)
-	(list temp 'c 'is (+ (* 1.8 temp) 32) 'f)
-	(list temp 'f 'is (/ (- temp 32) 1.8) 'c)
-	)
+    (list temp 'c 'is (+ (* 1.8 temp) 32) 'f)
+    (list temp 'f 'is (/ (- temp 32) 1.8) 'c)
+    )
   )
 
 (defun maxx (x y z)
   (if (>= x y)
-	(if (>= z x) z x)
-	(if (>= z y) z y)
-	)
+    (if (>= z x) z x)
+    (if (>= z y) z y)
+    )
   )
 
 (defun divisible-by (n m)
   (if (zerop (rem n m))
-	(list n 'is 'divisible 'by m)
-	(list n 'is 'not 'divisible 'by m)
-	)
+    (list n 'is 'divisible 'by m)
+    (list n 'is 'not 'divisible 'by m)
+    )
   )
 
 (defun my-member (x lst)
   (if (member x lst)
-	(list x 'is 'a 'member 'of lst)
-	(list x 'is 'not 'a 'member 'of lst)
-	)
+    (list x 'is 'a 'member 'of lst)
+    (list x 'is 'not 'a 'member 'of lst)
+    )
   )
 
 (defun order (x lst)
@@ -34,9 +34,9 @@
 
 (defun order2 (x lst)
   (if (null (member x lst))
-	nil
-	(+ (- (length lst) (length (member x lst))) 1)
-	)
+    nil
+    (+ (- (length lst) (length (member x lst))) 1)
+    )
   )
 
 (defun palindromep (lst)
@@ -45,10 +45,10 @@
 
 (defun dummy (x y)
   (if (endp y)
-	nil
-	(if (equal x (car y))
-	  y
-	  (dummy x (cdr y))
-	  )
-	)
+    nil
+    (if (equal x (car y))
+      y
+      (dummy x (cdr y))
+      )
+    )
   )
--- code/assignment-08.lisp
+++ code/assignment-08.lisp
@@ -1,32 +1,32 @@
 (defun blank-n (n lst &optional (counter 1))
   (cond ((endp lst) nil)
-		((= counter n) (cons 'x (blank-n n (cdr lst) 1)))
-		(t (cons (car lst) (blank-n n (cdr lst) (+ counter 1))))))
+        ((= counter n) (cons 'x (blank-n n (cdr lst) 1)))
+        (t (cons (car lst) (blank-n n (cdr lst) (+ counter 1))))))
 
 (defun c-find (x lst &optional (backup 0) (guess 0))
   "Find the position of the last occurrence of x in lst"
   (cond ((endp lst) guess)
-		((equal x (car lst)) (c-find x (cdr lst) (+ 1 backup) (+ 1 backup)))
-		(t (c-find x (cdr lst) (+ 1 backup) guess))))
+        ((equal x (car lst)) (c-find x (cdr lst) (+ 1 backup) (+ 1 backup)))
+        (t (c-find x (cdr lst) (+ 1 backup) guess))))
 
 ; a naive method
 (defun nsub-list (x y)
   (cond 
-	; admit failure if y gets shorter than x
-	((< (length y) (length x)) nil)
-	; empty list is a nsub-list of every list 
-	; so succeed if you manage to reduce x to nil
-	; or find it as such from the start.
-	; also note that the cond element only has a test -- no further elements
-	; if you do not provide anything after the test, T is returned if the test succeeds 
-	((endp x))
-	; if you have both lists starting with the same element
-	; go on with the cdrs of both x and y
-	((equal (car x) (car y)) 
-	 (nsub-list (cdr x) (cdr y)))
-	; if the first elements do not match, 
-	; try x with  the cdr of y 
-	(t (nsub-list x (cdr y)))))
+    ; admit failure if y gets shorter than x
+    ((< (length y) (length x)) nil)
+    ; empty list is a nsub-list of every list 
+    ; so succeed if you manage to reduce x to nil
+    ; or find it as such from the start.
+    ; also note that the cond element only has a test -- no further elements
+    ; if you do not provide anything after the test, T is returned if the test succeeds 
+    ((endp x))
+    ; if you have both lists starting with the same element
+    ; go on with the cdrs of both x and y
+    ((equal (car x) (car y)) 
+     (nsub-list (cdr x) (cdr y)))
+    ; if the first elements do not match, 
+    ; try x with  the cdr of y 
+    (t (nsub-list x (cdr y)))))
 
 
 
@@ -35,12 +35,12 @@
 ; whether you have already started matching lists
 (defun sub-list (x y &optional (in-match nil))
   (cond 
-	((< (length y) (length x)) nil)
-	((endp x) in-match)
-	((equal (car x) (car y)) 
-	 (or (sub-list (cdr x) (cdr y) t)
-		 (sub-list x (cdr y)) nil))
-	(t (if in-match
-		 nil
-		 (sub-list x (cdr y) nil)))))
+    ((< (length y) (length x)) nil)
+    ((endp x) in-match)
+    ((equal (car x) (car y)) 
+     (or (sub-list (cdr x) (cdr y) t)
+         (sub-list x (cdr y)) nil))
+    (t (if in-match
+         nil
+         (sub-list x (cdr y) nil)))))
 
