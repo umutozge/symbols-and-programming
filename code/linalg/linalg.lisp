ff8f246d02c66f53debb324c1362943af89e0e86
Umut Ozge <umut.oezge@gmail.com>
2023-12-26 12:13:38 +0300

    linalg.lisp updated

--- code/linalg/linalg.lisp
+++ code/linalg/linalg.lisp
@@ -23,3 +23,50 @@
   (or
     (format t "~{~{~3D~^ ~}~%~}~%" matrix)
     matrix))
+
+(defun range (n)
+  (reverse (_range n))
+  )
+(defun _range (n)
+  (if (= n 0)
+      nil
+      (cons (- n 1) (_range (- n 1)))))
+
+(defun mat-transpose (matrix)
+  (mapcar
+    #'(lambda (x)
+        (mapcar
+          #'(lambda (lst)
+              (nth x lst)
+              )
+          matrix))
+   (range (length (car matrix)))))
+
+(defun mat-trans (matrix)
+  (apply #'mapcar #'list matrix))
+
+(defun unit-prod (left right)
+ (apply
+   #'+
+   (mapcar
+     #'*
+     left
+     right)))
+
+(defun mat-prod (left right)
+  (mapcar
+    #'(lambda (x)
+     (mapcar
+       #'(lambda (y)
+           (unit-prod x y))
+       (mat-transpose right)))
+    left))
+
+;; Check wether the following variant works fine or not, and why?
+(defun mat-prod-alt (left right)
+  (mapcar
+    #'(lambda (x)
+     (mapcar
+       #'unit-prod x
+       (mat-transpose right)))
+    left))
